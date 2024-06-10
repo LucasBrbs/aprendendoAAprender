e7756df5639c3f8642398ee6144c00da4c5dd08d
Lucas  Barbosa de Oliveira <lucasbarbosadeoliveira@MacBook-Pro-de-Lucas.local>
2024-06-10 19:14:51 -0300

    test

---
+++ aprendendo/nome/nome/nomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nomeApp.swift
+//  nome
+//
+//  Created by Lucas  Barbosa de Oliveira on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct nomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
